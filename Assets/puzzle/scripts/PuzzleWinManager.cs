4217ddbdde2bb3eddfc21a61f4156965e214d61d
gn24102006-droid <gn24102006@gmail.com>
2026-01-15 22:41:40 +0700

    trang cuối nhật kí
    trang cuối nhật kí rơi ra sau khi chơi xong puzzle

--- Assets/puzzle/scripts/PuzzleWinManager.cs
+++ Assets/puzzle/scripts/PuzzleWinManager.cs
@@ -11,8 +11,6 @@ public class PuzzleWinManager : MonoBehaviour
     public GameObject winImage;
     public float winDelay = 2.5f;
 
-    public static System.Action onCompletePuzzle;
-
     void Awake()
     {
         Instance = this;
@@ -30,24 +28,22 @@ public class PuzzleWinManager : MonoBehaviour
 
     IEnumerator WinSequence()
     {
-        // 1. Đặt Flag chiến thắng
         PlayerPrefs.SetInt("PuzzleFinished", 1);
-        PlayerPrefs.Save(); // Đảm bảo dữ liệu được ghi xuống đĩa ngay lập tức
-        Debug.Log("Đã lưu cờ chiến thắng Puzzle!");
-        // 2. Hiển thị hiệu ứng Win
+        PlayerPrefs.Save();
+
         winImage.SetActive(true);
         winImage.transform.localScale = Vector3.zero;
+
         float t = 0f;
         while (t < 1f)
         {
             t += Time.deltaTime * 2f;
-            winImage.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, t);
+            winImage.transform.localScale =
+                Vector3.Lerp(Vector3.zero, Vector3.one, t);
             yield return null;
         }
 
         yield return new WaitForSeconds(winDelay);
-
-        // 3. Tải cảnh SampleScene nơi có Ông Nội
         SceneManager.LoadScene("SampleScene");
     }
 }
